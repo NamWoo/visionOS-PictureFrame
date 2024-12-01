0514d04bb8c33f25b3457f6e7575dd366388a1f3
NamWoo <pre3ice@gmail.com>
2024-12-01 18:54:34 +0900

    init

---
+++ PictureFrame/PictureFrame/PictureFrameApp.swift
@@ -0,0 +1,33 @@
+//
+//  PictureFrameApp.swift
+//  PictureFrame
+//
+//  Created by namu.kim on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PictureFrameApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ PictureFrame/PictureFrameTests/PictureFrameTests.swift
@@ -0,0 +1,17 @@
+//
+//  PictureFrameTests.swift
+//  PictureFrameTests
+//
+//  Created by namu.kim on 12/1/24.
+//
+
+import Testing
+@testable import PictureFrame
+
+struct PictureFrameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
